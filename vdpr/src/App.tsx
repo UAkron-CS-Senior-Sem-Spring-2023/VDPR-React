2882db483adc94938bfb9656fb14059db51eb89f
Nathan M. Hulet <nmh91@uakron.edu>
2023-02-21 00:59:56 -0500

    Add Chakra-ui to project

--- vdpr/src/App.tsx
+++ vdpr/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Heading,
+  VStack,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={5}>
+          <Heading>
+            Welcome to VDPR
+          </Heading>
+        </VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
