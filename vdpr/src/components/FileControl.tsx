9994a8e5ed5ce1fbc2b90e464a7bd0a732ab1465
Nathan M. Hulet <nmh91@uakron.edu>
2023-03-11 12:26:48 -0500

    Implement new file uploader with axios for api

---
+++ vdpr/src/components/FileControl.tsx
@@ -0,0 +1,89 @@
+import { useCallback } from "react";
+import { unstable_batchedUpdates } from "react-dom";
+import { useDropzone } from "react-dropzone";
+import axios from "axios";
+import { Flex, Button, useColorMode } from "@chakra-ui/react";
+
+interface DropzoneProps {
+  loading: boolean;
+  setLoading: (bool: boolean) => void;
+  setTaskId: (taskId: string | null) => void;
+  setError: (error: string | null) => void;
+}
+export default function Dropzone({
+  loading,
+  setLoading,
+  setTaskId,
+  setError,
+}: DropzoneProps) {
+  const { colorMode } = useColorMode();
+
+  const onDrop = useCallback(
+    (acceptedFiles: (string | Blob)[]) => {
+      let formData = new FormData();
+      formData.append("uploadfile", acceptedFiles[0]);
+
+      axios({
+        method: "POST",
+        url: process.env.NEXT_PUBLIC_HEROKU_PREDICT_URL,
+        // url: "http://localhost:5000/predict",
+        data: formData,
+      })
+        .then((res) => {
+          // force batch update to prevent extra render
+          unstable_batchedUpdates(() => {
+            setTaskId(res.data.task_id);
+            setLoading(true);
+          });
+        })
+        .catch((err) => {
+          setTaskId(null);
+          setLoading(false);
+          setError(err.message);
+        });
+    },
+    [setTaskId, setError, setLoading]
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: {
+      'image/pdf': ['.pdf']
+    },
+    disabled: loading,
+  });
+
+  return (
+    <div {...getRootProps({ className: "dropzone disabled" })}>
+      <Flex
+        flexDirection="column"
+        alignItems="center"
+        py={40}
+        borderWidth={4}
+        borderRadius={6}
+        // borderStyle="dashed"
+        borderColor={colorMode === "light" ? "gray.300" : "gray.400"}
+        bgColor={colorMode === "light" ? "gray.200" : "gray.500"}
+        outline="none"
+      >
+        <input {...getInputProps()} formEncType="multipart/form-data" />
+        {isDragActive ? (
+          <p>Drag and drop files here...</p>
+        ) : (
+          <>
+            {loading ? null : <p>Upload your transcript to get started</p>}
+            <Button
+              colorScheme={loading ? "" : "teal"}
+              mt={2}
+              isLoading={loading}
+              loadingText="Analizing your progress..."
+              variant={loading ? "ghost" : "solid"}
+            >
+              Select File
+            </Button>
+          </>
+        )}
+      </Flex>
+    </div>
+  );
+}
--- vdpr/src/layouts/homePage.tsx
+++ vdpr/src/layouts/homePage.tsx
@@ -7,17 +7,17 @@ import {
   theme,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "../ColorModeSwitcher"
-import FileInput from "../components/FileInput"
+import FileUpload from "../components/FileUpload"
 
 const StartPage = () => {
   return (
   <Box textAlign="center" fontSize="xl">
-    <Grid minH="100vh" p={3}>
+    <Grid minH="25vh" p={3}>
       <ColorModeSwitcher justifySelf="flex-end" />
-      <VStack spacing={5}>
+      <VStack spacing={3}>
         <Heading>
           Welcome to VDPR
-          <FileInput />
+          <FileUpload />
         </Heading>
       </VStack>
     </Grid>
